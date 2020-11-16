c142e7df0924c82788439e86b99277901c21540c
Kozlov Kolia <koliakozlov@yahoo.com>
2020-11-16 22:06:52 +0300

    1_draw

---
+++ lab3/pygame_events.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+
+FPS = 60
+W = 700  # ширина экрана
+H = 300  # высота экрана
+WHITE = (255, 255, 255)
+BLUE = (0, 70, 225)
+
+sc = pygame.display.set_mode((W, H))
+clock = pygame.time.Clock()
+
+# координаты и радиус круга
+x = W // 2
+y = H // 2
+r = 50
+
+while 1:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            sys.exit()
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_LEFT:
+                x -= 3
+            elif i.key == pygame.K_RIGHT:
+                x += 3
+
+    sc.fill(WHITE)
+    pygame.draw.circle(sc, BLUE, (x, y), r)
+    pygame.display.update()
+    clock.tick(FPS)
